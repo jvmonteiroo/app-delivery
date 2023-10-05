cfb08b645cbdaba3dd7c4379168fa819d5551747
david <dr4714@gmail.com>
2023-10-05 18:35:29 -0300

    aprendi navegação de TAB

--- App.tsx
+++ App.tsx
@@ -5,16 +5,18 @@ import Onboarding from './src/screens/Onboarding';
 import Home from './src/screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProductDetails from './src/screens/ProductDetails';
 
-const stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <stack.Navigator>
-        <stack.Screen name="Onboarding"component={Onboarding}/>
-        <stack.Screen name="Home"component={Home}/>
-      </stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Onboarding"component={Onboarding}/>
+        <Stack.Screen name="Home"component={Home}/>
+        <Stack.Screen name="ProductDetails"component={ProductDetails}/>
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
---
+++ src/screens/ProductDetails/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native"
+
+
+export default function ProductDetails (){
+    return(
+        <Text>Detalhes do produto</Text>
+    )
+}
