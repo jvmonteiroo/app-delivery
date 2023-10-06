4211e483fb01faf50c06e1c7802c1a11ba8de172
jvmonteiroo <144281056+jvmonteiroo@users.noreply.github.com>
2023-10-06 12:52:15 -0300

    nova tela ProductDetails se iniciando

--- images.ts
+++ images.ts
@@ -4,5 +4,7 @@ export const images = {
     logo:require("./src/images/logo.png"),
     pizza:require("./src/images/pizza.png"),
     group:require("./src/images/group.png"),
-    produto:require("./src/images/produto.png")
+    produto:require("./src/images/produto.png"),
+    Voltar:require("./src/images/Voltar.png"),
+    Favoritos:require("./src/images/Favoritos.png")
 }
--- src/screens/ProductDetails/index.tsx
+++ src/screens/ProductDetails/index.tsx
@@ -1,8 +1,31 @@
-import { Text } from "react-native"
+import { Text, View, StyleSheet, Image } from "react-native";
+import { images } from '../../../images';
 
 
 export default function ProductDetails (){
     return(
-        <Text>Detalhes do produto</Text>
+        <View style={styles.container}>
+            <View style={styles.parteDeCimaPorBaixo}>
+                <Image source={images.Voltar}/>
+                <Text>
+                    Prato - Massas
+                </Text>
+                <Image source={images.Favoritos}/>
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderWidth:10,
+        padding: 30,
+
+    },    
+    parteDeCimaPorBaixo: {
+        borderWidth:10,
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    }
+})
